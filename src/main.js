8e23b136ede046c40db92b07060169508d68be26
CharlesMassuard <charles.massuard@gmail.com>
2026-01-11 15:23:23 +0100

    Ajout de Vuetify

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import router from './router'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+app.use(vuetify)
+
+app.mount('#app')
