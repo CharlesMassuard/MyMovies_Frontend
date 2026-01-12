d81e59232568f601ee42c522aaaa39bc44fe2840
CharlesMassuard <charles.massuard@gmail.com>
2026-01-12 21:09:43 +0100

    Ajout films En Salles & refonte style

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -6,12 +6,17 @@
     const trendingMovies = ref([]);
     const inTheaterMovies = ref([]);
 
+    const date = new Date();
+    const currentYear = date.getFullYear();
+    const currentMonth = String(date.getMonth() + 1).padStart(2, '0');
+    const currentDay = String(date.getDate()).padStart(2, '0');
+    const currentDateString = `${currentYear}-${currentMonth}-${currentDay}`;
+
     const fetchTrendingMovies = async () => {
         try {
             const response = await fetch(`${apiPath}/movies/trending`);
             const data = await response.json();
             trendingMovies.value = data.results;
-            console.log("Trending Movies:", trendingMovies.value);
         } catch (error) {
             console.error('Error fetching trending movies:', error);
         }
@@ -22,6 +27,7 @@
             const response = await fetch(`${apiPath}/movies/in-theater`);
             const data = await response.json();
             inTheaterMovies.value = data.results;
+            console.log(data.results);
         } catch (error) {
             console.error('Error fetching in-theater movies:', error);
         }
@@ -36,42 +42,129 @@
 <template>
   <v-container fluid class="pa-0">
     <div class="py-8">
-      
-      <h2 class="text-h5 font-weight-bold mb-4 px-4">Les Tendances</h2>
+      <h1 class="text-h5 font-weight-bold mb-2 section-title">Les Tendances</h1>
 
       <v-slide-group :show-arrows="false" class="full-width-slide">
-        <v-slide-group-item v-for="movie in trendingMovies" :key="movie.id">
-          <v-card
-            class="ma-2 movie-card"
-            rounded="xl"
-            width="200"
-            v-tooltip="{ text: movie.title, openDelay: 500, location: 'bottom' }"
-          >
-            <v-img
-              :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`"
-              cover
-              aspect-ratio="2/3"
-            ></v-img>
-          </v-card>
+        <v-slide-group-item v-for="(movie, index) in trendingMovies" :key="movie.id">
+          <div class="card-container ma-4">
+            
+            <div class="border-wrapper">
+              <v-card
+                class="movie-card"
+                rounded="l"
+                width="150"
+                flat
+                v-tooltip="{ text: movie.title, openDelay: 500, location: 'bottom' }"
+              >
+                <v-img
+                  :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`"
+                  cover
+                  aspect-ratio="2/3"
+                  class="movie-img"
+                ></v-img>
+              </v-card>
+            </div>
+            
+            <span class="ranking-number">{{ index + 1 }}</span>
+          </div>
         </v-slide-group-item>
       </v-slide-group>
+    </div>
+    <div>
+      <h1 class="text-h5 font-weight-bold mb-2 section-title" title="Films sortis depuis 40 jours">En Salles</h1>
 
+      <v-slide-group :show-arrows="false" class="full-width-slide">
+        <v-slide-group-item v-for="(movie, index) in inTheaterMovies" :key="movie.id">
+          <div class="card-container ma-4" v-if="movie.release_date <= currentDateString">
+            
+            <div class="border-wrapper">
+              <v-card
+                class="movie-card"
+                rounded="l"
+                width="150"
+                flat
+                v-tooltip="{ text: movie.title, openDelay: 500, location: 'bottom' }"
+              >
+                <v-img
+                  :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`"
+                  cover
+                  aspect-ratio="2/3"
+                  class="movie-img"
+                ></v-img>
+              </v-card>
+            </div>
+            <span class="ranking-number">{{ index + 1 }}</span>
+          </div>
+        </v-slide-group-item>
+      </v-slide-group>
     </div>
   </v-container>
 </template>
 
 <style scoped>
     .full-width-slide {
-        width: 100vw;
+        margin-left: 30px;
+        width: 95.5vw;
+    }
+
+    .section-title {
+        position: relative;
+        margin-left: 30px !important;
+        padding-bottom: 15px;
+        display: inline-block;
+    }
+
+    .section-title::after {
+        content: "";
+        position: absolute;
+        bottom: 0;
+        left: 0; 
+        width: 95.5vw; 
+        height: 2px;
+        background-color: #ebebeb;
+    }
+
+    .card-container {
+        position: relative;
+        padding-bottom: 10px;
+    }
+
+    .border-wrapper {
+        border: 4px solid #ffffff;
+        border-radius: 20px;
+        overflow: hidden;
+        width: fit-content;
+        z-index: 1;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.4);
     }
 
     .movie-card {
-        transition: transform 0.2s;
         cursor: pointer;
+        background: transparent;
+    }
+    .movie-img {
+        transition: transform 0.3s ease;
+        width: 150px;
+        height: 225px;
     }
 
-    .movie-card:hover {
-        transform: scale(1.05);
+    .movie-card:hover .movie-img {
+        transform: scale(1.1);
+    }
+
+    .ranking-number {
+        position: absolute;
+        bottom: -10px;
+        left: -25px;
+        font-size: 6rem;
+        font-weight: 900;
+        line-height: 1;
+        z-index: 2;
+        color: #e1e1e1;
+        text-shadow: 2px 2px 10px rgba(0,0,0,0.3);
+        -webkit-text-stroke: 4px #ffffff;
+        user-select: none;
+        pointer-events: none;
     }
 
     :deep(.v-slide-group__content) {
