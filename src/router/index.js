454a72a1fdcdb07e466fd2dcc3492cc5c1259d17
CharlesMassuard <charles.massuard@gmail.com>
2026-01-16 20:12:12 +0100

    Ajout page 404

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import LoginView from '../views/LoginView.vue'
 import MainView from '../views/MainView.vue'
 import { useAuthStore } from '../stores/auth';
 import MovieDetailsView from '../views/MovieDetailsView.vue';
+import NotFoundView from '../views/NotFoundView.vue';
 
 const routes = [
     {
@@ -23,6 +24,13 @@ const routes = [
         name: 'movieDetails',
         component: MovieDetailsView,
         meta: { public: true }
+    },
+    //404
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'notFound',
+      component: NotFoundView,
+      meta: { public: true, hideHeader: true }
     }
 ]
 
---
+++ src/views/MovieDetailsView.vue
@@ -0,0 +1,34 @@
+<script setup>
+  import { ref, onMounted } from 'vue';
+  import { useRoute } from 'vue-router';
+  import axios  from 'axios';
+  
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+  const movieId = useRoute().params.id;
+  
+  const fetchDetailsMovies = async () => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/movies/${movieId}`);
+      console.log('Movie details:', response.data);
+    } catch (error) {
+      console.error('Error fetching movie details:', error);
+    }
+  };
+
+  onMounted(() => {
+    fetchDetailsMovies();
+  });
+
+</script>
+
+<template>
+  <v-container fluid class="pa-4">
+    <h1>{{ msg }}</h1>
+    <v-btn @click="count++">You clicked me {{ count }} times.</v-btn>
+  </v-container>
+</template>
+
+<style scoped>
+
+</style>
