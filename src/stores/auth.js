e48d1bda8ea480527b40029d3cf2529d3e5c2a2d
CharlesMassuard <charles.massuard@gmail.com>
2026-09-11 11:42:08 +0200

    Ajout refreshToken

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -3,15 +3,19 @@ import { ref, computed } from 'vue';
 
 export const useAuthStore = defineStore('auth', () => {
   const token = ref(localStorage.getItem('user_token') || null);
+  const refreshToken = ref(localStorage.getItem('user_refresh_token') || null);
   
   const savedUser = localStorage.getItem('user_data');
   const user = ref(savedUser ? JSON.parse(savedUser) : null);
 
   const isAuthenticated = computed(() => !!token.value);
 
-  function login(newToken, userData) {
+  function login(newToken, newRefreshToken, userData) {
     token.value = newToken;
+    refreshToken.value = newRefreshToken;
     localStorage.setItem('user_token', newToken);
+    localStorage.setItem('user_refresh_token', newRefreshToken);
+    
     if (userData) {
       user.value = userData;
       localStorage.setItem('user_data', JSON.stringify(userData));
@@ -25,10 +29,12 @@ export const useAuthStore = defineStore('auth', () => {
 
   function logout() {
     token.value = null;
+    refreshToken.value = null;
     user.value = null;
     localStorage.removeItem('user_token');
+    localStorage.removeItem('user_refresh_token');
     localStorage.removeItem('user_data');
   }
 
-  return { token, user, isAuthenticated, login, setUser, logout };
+  return { token, refreshToken, user, isAuthenticated, login, setUser, logout };
 });
